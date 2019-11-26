ac42298bec735a42dcc3adcf080776f7e43aea62
Alexander Sobolev <sobolev.ax@gmail.com>
2019-11-26 23:13:50 +0300

    chapter 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharp_metanit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
